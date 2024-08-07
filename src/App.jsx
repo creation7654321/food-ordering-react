132ea29e24045542154eba1f404ff0070e5b6e3f
creation7654321 <creation.pradhan@gmail.com>
2024-08-07 21:22:31 +0545

    initializing browser router and creating routes for home, car and placeorder page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 export default function App() {
   return (
     <div className='app'>
       <Navbar />
+      <Home />
+      <Routes >
+        <Route to="/" element={<Home/>} />
+        <Route to="/cart" element={<Cart/>} />
+        <Route to="/order" element={<PlaceOrder/>} />
+      </Routes>
     </div>
   )
 }
