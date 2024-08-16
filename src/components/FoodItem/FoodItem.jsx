299b48cecf5cff8fb40906ff6ddf4e742fc1099f
creation7654321 <creation.pradhan@gmail.com>
2024-08-16 10:59:17 +0545

    adding an icon below the image so that user and increase or decrease the price of that specific item and the value of that item is changed using setItemCount

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
 export default function FoodItem({ id, name, price, description, image }) {
+
+
+  const [itemCount, setItemCount] = useState(0)
+
+
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img className="food-item-image" src={image} alt="" />
+        {!itemCount
+        ?
+        <img 
+        src={assets.add_icon_white}
+        className="add"
+        alt=""
+        onClick={()=>setItemCount(prev=>prev+1)}
+        />
+        :
+        <div className="food-item-counter">
+          <img src={assets.remove_icon_red}
+            onClick={()=>setItemCount(prev=>prev-1)}
+          />
+            {itemCount}
+          <img src={assets.add_icon_green}
+            onClick={()=>setItemCount(prev=>prev+1)}
+          />
+
+          
+        </div>
+        }
+
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
