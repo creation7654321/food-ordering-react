9c3fabf37a3f37af5a28b054a146c0ad4c50fefe
creation7654321 <creation.pradhan@gmail.com>
2024-08-17 16:16:47 +0545

    adding basic cart functionality with addToCart, and removeFromCart. State management for cart items is handled using useState hook

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,32 +1,34 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 export default function FoodItem({ id, name, price, description, image }) {
 
 
-  const [itemCount, setItemCount] = useState(0)
+
+  const {cartItems, addToCart, removeFromCart} = useContext(StoreContext);
 
 
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img className="food-item-image" src={image} alt="" />
-        {!itemCount
+        {!cartItems[id]
         ?
         <img 
         src={assets.add_icon_white}
         className="add"
         alt=""
-        onClick={()=>setItemCount(prev=>prev+1)}
+        onClick={()=>addToCart(id)}
         />
         :
         <div className="food-item-counter">
           <img src={assets.remove_icon_red}
-            onClick={()=>setItemCount(prev=>prev-1)}
+            onClick={()=>removeFromCart(id)}
           />
-            {itemCount}
+            {cartItems[id]}
           <img src={assets.add_icon_green}
-            onClick={()=>setItemCount(prev=>prev+1)}
+            onClick={()=>addToCart(id)}
           />
 
           
